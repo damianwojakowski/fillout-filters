121d1be3e66a0391ed80e26390d95237cf84a2a8
Damian Wojakowski <wojakowski.damian@gmail.com>
2024-03-04 15:53:19 +0100

    code refactoring

--- src/controllers/FormResponseController.ts
+++ src/controllers/FormResponseController.ts
@@ -7,16 +7,15 @@ import { APIQueryParams } from '../models/ApiQueryParams';
 
 export class FormResponsesController {
     static async getFilteredResponses(req: Request, res: Response) {
-        const { formId } = req.params;
-        const queryParams: APIQueryParams = FormReponseUtility.getQueryParameters(req);
-        const filters: FilterClauseType[] = FormReponseUtility.getFilters(req);
-
         try {
+            const { formId } = req.params;
+            const queryParams: APIQueryParams = FormReponseUtility.getQueryParameters(req);
+            const filters: FilterClauseType[] = FormReponseUtility.getFilters(req);
             const filteredResponses = await FormResponsesService.fetchFilteredResponses(formId, queryParams, filters);
             res.json(filteredResponses);
         } catch (error) {
             console.error(error);
-            res.status(500).json({ message: error });
+            res.status(500).json({ message: "Request failed. Make sure you're providing correct parameters." });
         }
     }
 
